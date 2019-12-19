cc8d9edff7ae97623f0ff8d6f9b8b481c47c652a
Maxime Sainlot <maxime.sainlot@gmail.com>
2019-12-19 13:51:09 +1300

    change static url for server deployment

--- webinterface/settings.py
+++ webinterface/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/patacrep/static/'
 
 # https://docs.djangoproject.com/en/1.8/howto/static-files/deployment/
 # python manage.py collectstatic
