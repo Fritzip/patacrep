716e71cac7f88a41668058f1847f3ebc997587f2
Maxime Sainlot <maxime.sainlot@gmail.com>
2020-08-28 17:53:37 +1200

    [DIRTY] hide header on scroll and removed sidebar

--- patacrep/context_processors.py
+++ patacrep/context_processors.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 from .models import Chord
 
-def artists(request):
-    artist_list = list(set(map(lambda x : x['artist'], list(Chord.objects.values('artist')))))
-    artist_list.sort()
-    artist_list = [(artist,  Chord.objects.filter(artist=artist).count()) for artist in artist_list]
-    return {'artist_list': artist_list}
+# def artists(request):
+#     artist_list = list(set(map(lambda x : x['artist'], list(Chord.objects.values('artist')))))
+#     artist_list.sort()
+#     artist_list = [(artist,  Chord.objects.filter(artist=artist).count()) for artist in artist_list]
+#     return {'artist_list': artist_list}
